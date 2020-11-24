abc826eba900055dfe0f5486243b589ebb6eab12
Dario <darioalessio79@gmail.com>
2020-11-24 17:03:51 +0100

    inserirre testo fatto , risposta automatica "ok" messa

--- js/script.js
+++ js/script.js
@@ -2,15 +2,26 @@
 var app = new Vue({
   el:"#app",
   data:{
+    textUser:'',
+    textPc:'ok',
     attivo: 0,
     nameLists:[{
       name:'Michele',
       lastAccess:'10/10/2020, 20:59',
       image: 'https://www.britishinstitutes.it/sedi/skin/default-avatar.jpg',
       selection:[
-        {message:'Come ti chiami ?'},
-        {message:'Sono Filippo'},
-        {message:'Piacere di conoscerti'}
+        {
+          message:'Come ti chiami ?',
+          stato:'inviato'
+        },
+        {
+          message:'Sono Filippo',
+          stato:'ricevuto'
+        },
+        {
+          message:'Piacere di conoscerti',
+          stato:'inviato'
+        }
       ]
     },
     {
@@ -18,9 +29,18 @@ var app = new Vue({
       lastAccess:'10/10/2020, 10:57',
       image: 'https://www.britishinstitutes.it/sedi/skin/default-avatar.jpg',
       selection:[
-        {message:'che cosa volete da mangiare ?'},
-        {message:'pizzaaaaaaa'},
-        {message:'perfettooo!!!'}
+        {
+          message:'che cosa volete da mangiare ?',
+          stato:'inviato'
+        },
+        {
+          message:'pizzaaaaaaa',
+          stato:'ricevuto'
+        },
+        {
+          message:'perfettooo!!!',
+          stato:'inviato'
+        }
       ]
     },
     {
@@ -28,9 +48,18 @@ var app = new Vue({
       lastAccess:'10/10/2020, 19:44',
       image: 'https://www.britishinstitutes.it/sedi/skin/default-avatar.jpg',
       selection:[
-        {message:'Allora caldi per il calcetto ?'},
-        {message:'Manca un giocatore...'},
-        {message:'Meraviglioso...perderemo sicuramente'}
+        {
+          message:'Allora caldi per il calcetto ?',
+          stato:'inviato'
+        },
+        {
+          message:'Manca un giocatore...',
+          stato:'ricevuto'
+        },
+        {
+          message:'Meraviglioso...perderemo sicuramente',
+          stato:'inviato'
+        }
       ]
     },
     {
@@ -38,15 +67,43 @@ var app = new Vue({
     lastAccess:'10/10/2020, 02:59',
     image: 'https://www.britishinstitutes.it/sedi/skin/default-avatar.jpg',
     selection:[
-      {message:' Fiestaaaaa'},
-      {message:'Sei ubriaco??'},
-      {message:'Alllaaaaa ggggrandeeee'}
+      {
+        message:' Fiestaaaaa',
+        stato:'inviato'
+      },
+      {
+        message:'Sei ubriaco??',
+        stato:'ricevuto'
+      },
+      {
+        message:'Alllaaaaa ggggrandeeee',
+        stato:'inviato'
+      }
     ]
 
     }
 
     ]
+  },
+  // inserire testo input funzione
+  methods :{
+    addText: function(){
+      if(this.textUser!=''){
+        let newMess = {message: this.textUser,  stato: 'inviato'};
+        this.nameLists[this.attivo].selection.push(newMess);
+        setTimeout(this.risposta, 1000);
+        this.attivo = 0;
+        this.textUser = "";
+      }
+
+    },
+    // funzione per definire la risposta automatica del pc
+    risposta: function() {
+       let pcReturn = {message: this.textPc , stato: 'ricevuto'};
+       this.nameLists[this.attivo].selection.push(pcReturn);
+   }
   }
+
 
 
   });
